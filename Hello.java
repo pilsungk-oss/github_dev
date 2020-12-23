81c907fc641d41f5408eb97be687454f8a25d1c9
Pilsung Kang <oss.psk@gmail.com>
2020-12-23 16:11:50 +0900

    Add GUI.java, Change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         Sytem.out.println("Hello World");
-        Sytem.out.println("Second Hello World");
+        Sytem.out.println("GUI Hello World");
     }
 }
