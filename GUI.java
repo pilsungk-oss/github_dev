245a576b66418a87fc263c40d12daabbf26842ab
Pilsung Kang <pilsungk+oss@gmail.com>
2020-12-24 12:09:11 +0900

    Change GUI.java: add a new println message

--- GUI.java
+++ GUI.java
@@ -2,5 +2,6 @@ public class GUI {
     public static void main(String[] args) {
         Sytem.out.println("GUI branch");
         Sytem.out.println("Second message: GUI branch");
+        Sytem.out.println("Third message: GUI branch");
     }
 }
